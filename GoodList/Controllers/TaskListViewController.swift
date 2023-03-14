1f8ce2ca56e47d3fea63449fdb072829cee3ebee
Òscar Muntal <oscar.muntal@gmail.com>
2023-03-14 15:35:55 +0100

    [TaskList]: Filtering task items by priority (WIP)

--- GoodList/Controllers/TaskListViewController.swift
+++ GoodList/Controllers/TaskListViewController.swift
@@ -12,7 +12,7 @@ import RxCocoa
 class TaskListViewController: UIViewController {
     let disposeBag = DisposeBag()
     private var tasks = BehaviorRelay<[Task]>(value: [])
-    
+    private var filteredTasks = [Task]()
     @IBOutlet weak var prioritySegmentedControl: UISegmentedControl!
     @IBOutlet weak var tableView: UITableView!
     
@@ -44,10 +44,27 @@ extension TaskListViewController: UITableViewDelegate, UITableViewDataSource {
               let addTaskVC = navigationController.viewControllers.first as? AddTaskViewController else { fatalError("Controller not found...") }
         
         addTaskVC.taskSubjectObservable
-            .subscribe(onNext: { task in
+            .subscribe(onNext: { [unowned self] task in
+                let priority = Priority(rawValue: self.prioritySegmentedControl.selectedSegmentIndex - 1)
                 var existingTasks = self.tasks.value
                 existingTasks.append(task)
                 self.tasks.accept(existingTasks)
+                self.filterTasks(by: priority)
             }).disposed(by: disposeBag)
+    }
+    
+    private func filterTasks(by priority: Priority?) {
+        // Case: All
+        if priority == nil {
+            self.filteredTasks = self.tasks.value
+        } else {
+            // Cases: High, Medium and Low
+            self.tasks.map { tasks in
+                tasks.filter { $0.priority == priority! }
+            }.subscribe(onNext:{ [weak self] tasks in
+                self?.filteredTasks = tasks
+                print(tasks)
+            }).disposed(by: disposeBag)
+        }
     }
 }
