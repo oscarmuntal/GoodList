dbd5cd2c3513e0845ef3dab34dbd80dc126373e4
Òscar Muntal <oscar.muntal@gmail.com>
2023-03-14 15:07:15 +0100

    [AddTask]: Adding the task publisher and the task observable

--- GoodList/Controllers/AddTaskViewController.swift
+++ GoodList/Controllers/AddTaskViewController.swift
@@ -7,8 +7,14 @@
 
 import Foundation
 import UIKit
+import RxSwift
 
 class AddTaskViewController: UIViewController {
+    private let taskSubject = PublishSubject<Task>()
+    public var taskSubjectObservable: Observable<Task> {
+        return taskSubject.asObservable()
+    }
+    
     @IBOutlet weak var prioritySegmentedControl: UISegmentedControl!
     @IBOutlet weak var taskTitleTextField: UITextField!
     
@@ -17,5 +23,9 @@ class AddTaskViewController: UIViewController {
               let title = taskTitleTextField.text else { return }
         
         let task = Task(title: title, priority: priority)
+        taskSubject.onNext(task)    // sending the task we've created to the subject
+        // whoever is subscribed to this subject will the this particular task
+        
+        dismiss(animated: true, completion: nil)
     }
 }
--- GoodList/Controllers/TaskListViewController.swift
+++ GoodList/Controllers/TaskListViewController.swift
@@ -5,10 +5,12 @@
 //  Created by Òscar Muntal on 14/3/23.
 //
 
-import Foundation
 import UIKit
+import RxSwift
 
 class TaskListViewController: UIViewController {
+    let disposeBag = DisposeBag()
+    
     @IBOutlet weak var prioritySegmentedControl: UISegmentedControl!
     @IBOutlet weak var tableView: UITableView!
     
@@ -33,5 +35,15 @@ extension TaskListViewController: UITableViewDelegate, UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TaskTableViewCell", for: indexPath)
         
         return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let navigationController = segue.destination as? UINavigationController,
+              let addTaskVC = navigationController.viewControllers.first as? AddTaskViewController else { fatalError("Controller not found...") }
+        
+        addTaskVC.taskSubjectObservable
+            .subscribe(onNext: { task in
+                print(task)
+            }).disposed(by: disposeBag)
     }
 }
