1c2695969752a99bb10298b1fcfd2ad2310efc87
Òscar Muntal <oscar.muntal@gmail.com>
2023-03-14 14:37:49 +0100

    [TaskList]: 'Add' button to create new tasks and larger title

--- GoodList/Controllers/TaskListViewController.swift
+++ GoodList/Controllers/TaskListViewController.swift
@@ -9,9 +9,13 @@ import Foundation
 import UIKit
 
 class TaskListViewController: UIViewController {
-    
     @IBOutlet weak var prioritySegmentedControl: UISegmentedControl!
     @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
     
 }
 
