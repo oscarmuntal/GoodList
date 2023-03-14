ce913c2b3f875a41c86eeee9784b32d53cdf3983
Òscar Muntal <oscar.muntal@gmail.com>
2023-03-14 14:43:31 +0100

    [AddTask]: AddTask VC added and it's UI

---
+++ GoodList/Controllers/AddTaskViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AddTaskViewController.swift
+//  GoodList
+//
+//  Created by Òscar Muntal on 14/3/23.
+//
+
+import Foundation
+import UIKit
+
+class AddTaskViewController: UIViewController {
+    
+}
