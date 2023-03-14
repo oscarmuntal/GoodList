61da0ae0c8189295496caf8f94058f03e35aa24b
Òscar Muntal <oscar.muntal@gmail.com>
2023-03-14 14:32:25 +0100

    [TaskList]: Main tasks list UI

---
+++ GoodList/Controllers/TaskListViewController.swift
@@ -0,0 +1,33 @@
+//
+//  TaskListViewController.swift
+//  GoodList
+//
+//  Created by Òscar Muntal on 14/3/23.
+//
+
+import Foundation
+import UIKit
+
+class TaskListViewController: UIViewController {
+    
+    @IBOutlet weak var prioritySegmentedControl: UISegmentedControl!
+    @IBOutlet weak var tableView: UITableView!
+    
+}
+
+extension TaskListViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskTableViewCell", for: indexPath)
+        
+        return cell
+    }
+}
